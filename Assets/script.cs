48e71e677c7ce95d5640eed3217c084608a74d42
pixeguy <168566265+pixeguy@users.noreply.github.com>
2025-01-25 16:42:45 +0800

    changes
    changes

--- Assets/script.cs
+++ Assets/script.cs
@@ -1,10 +1,14 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+=======
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class script : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public float speed;
     private float move;
     private float updown;
@@ -13,11 +17,24 @@ public class script : MonoBehaviour
     void Start()
     {
         rb=  GetComponent<Rigidbody2D>();
+=======
+
+    public float speed;
+    private float movex;
+    private float movey;
+
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         move = Input.GetAxis("Horizontal");
         updown = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(move * speed, updown * speed);
@@ -26,5 +43,11 @@ public class script : MonoBehaviour
         {
             print("interacting");
         }
+=======
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(movex * speed, movey * speed);
+>>>>>>> Stashed changes
     }
 }
